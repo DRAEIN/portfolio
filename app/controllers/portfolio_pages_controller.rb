f8d025471d9861fd1261563f17d4c9f794a0199c
Mark Billes <mark.billes@web.de>
2017-07-06 02:31:36 +0200

    added new and create actions for portfolio items

--- app/controllers/portfolio_pages_controller.rb
+++ app/controllers/portfolio_pages_controller.rb
@@ -1,7 +1,30 @@
 class PortfolioPagesController < ApplicationController
 
   def index
-  	@portfolio_items = PortfolioPage.all
+  	@portfolio_item = PortfolioPage.all
+  end
+
+  def new
+  	@portfolio_item = PortfolioPage.new
+  end
+
+  def create
+  	@portfolio_item = PortfolioPage.new(portfolio_params)
+
+  	respond_to do |format|
+  		if @portfolio_item.save
+  			format.html { redirect_to portfolio_pages_path , notice: 'Your Portfolio Item is now live' }
+  		else
+  			format.html { render :new }
+  		end
+  	end
+  end
+
+
+  private
+
+  def portfolio_params
+  	params.require(:portfolio_page).permit(:title, :subtitle, :body)
   end
 end
 
